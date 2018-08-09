be52a0b2a2fcaaeeaeda41e07fe34b745a262eac
vthorsteinsson <vt@extrada.com>
2018-08-09 15:22:45 +0000

    Version 1.2.2

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ def read(*names, **kwargs):
 
 setup(
     name="reynir",
-    version="1.2.1",
+    version="1.2.2",
     license="GNU GPLv3",
     description="A natural language parser for Icelandic",
     long_description="%s\n%s"
