12485d19bc40d95b8fe25c2597b821e3d71ba835
Vilhjalmur Thorsteinsson <vt@extrada.com>
2019-07-22 16:33:41 +0000

    Fixed tests

--- test/test_reynir.py
+++ test/test_reynir.py
@@ -56,26 +56,26 @@ def test_bin():
 
     def f(word, case, stem, cat, func=None):
         meanings = b.lookup_case(word, case)
-        return [
+        return set([
             (m[4], m[5]) for m in meanings
             if m[0] == stem and m[2] == cat and (func is None or func(m[5]))
-        ]
+        ])
 
     def declension(stem, cat, func=None):
         result = []
         for case in ("NF", "ÞF", "ÞGF", "EF"):
-            wf_list = f(stem, case, stem, cat, func)
+            wf_list = list(f(stem, case, stem, cat, func))
             result.append(wf_list[0][0] if wf_list else "N/A")
         return tuple(result)
 
     bf = lambda b: ("FVB" in b and "ET" in b)
     assert(
         f("fjarðarins", "NF", "fjörður", "kk") ==
-        [('firðir', 'NFFT'), ('firðirnir', 'NFFTgr'), ('fjörður', 'NFET'), ('fjörðurinn', 'NFETgr')]
+        {('firðir', 'NFFT'), ('firðirnir', 'NFFTgr'), ('fjörður', 'NFET'), ('fjörðurinn', 'NFETgr')}
     )
     assert(
         f("breiðustu", "NF", "breiður", "lo", bf) ==
-        [('breiða', 'FVB-KVK-NFET'), ('breiða', 'FVB-HK-NFET'), ('breiði', 'FVB-KK-NFET')]
+        {('breiða', 'FVB-KVK-NFET'), ('breiða', 'FVB-HK-NFET'), ('breiði', 'FVB-KK-NFET')}
     )
     assert(
         b.lookup_case("fjarðarins", "NF", "kk", {"ET", "gr"}) ==
@@ -87,27 +87,27 @@ def test_bin():
     )
     assert(
         f("fjarðarins", "ÞF", "fjörður", "kk") ==
-        [('firði', 'ÞFFT'), ('firðina', 'ÞFFTgr'), ('fjörð', 'ÞFET'), ('fjörðinn', 'ÞFETgr')]
+        {('firði', 'ÞFFT'), ('firðina', 'ÞFFTgr'), ('fjörð', 'ÞFET'), ('fjörðinn', 'ÞFETgr')}
     )
     assert(
         f("breiðustu", "ÞF", "breiður", "lo", bf) ==
-        [('breiða', 'FVB-KK-ÞFET'), ('breiða', 'FVB-HK-ÞFET'), ('breiðu', 'FVB-KVK-ÞFET')]
+        {('breiða', 'FVB-KK-ÞFET'), ('breiða', 'FVB-HK-ÞFET'), ('breiðu', 'FVB-KVK-ÞFET')}
     )
     assert(
         f("fjarðarins", "ÞGF", "fjörður", "kk") ==
-        [('firði', 'ÞGFET'), ('firðinum', 'ÞGFETgr'), ('fjörðum', 'ÞGFFT'), ('fjörðunum', 'ÞGFFTgr')]
+        {('firði', 'ÞGFET'), ('firðinum', 'ÞGFETgr'), ('fjörðum', 'ÞGFFT'), ('fjörðunum', 'ÞGFFTgr')}
     )
     assert(
         f("breiðustu", "ÞGF", "breiður", "lo", bf) ==
-        [('breiða', 'FVB-KK-ÞGFET'), ('breiða', 'FVB-HK-ÞGFET'), ('breiðu', 'FVB-KVK-ÞGFET')]
+        {('breiða', 'FVB-KK-ÞGFET'), ('breiða', 'FVB-HK-ÞGFET'), ('breiðu', 'FVB-KVK-ÞGFET')}
     )
     assert(
         f("fjarðarins", "EF", "fjörður", "kk") ==
-        [('fjarða', 'EFFT'), ('fjarðanna', 'EFFTgr'), ('fjarðar', 'EFET'), ('fjarðarins', 'EFETgr')]
+        {('fjarða', 'EFFT'), ('fjarðanna', 'EFFTgr'), ('fjarðar', 'EFET'), ('fjarðarins', 'EFETgr')}
     )
     assert(
         f("breiðustu", "EF", "breiður", "lo", bf) ==
-        [('breiða', 'FVB-KK-EFET'), ('breiða', 'FVB-HK-EFET'), ('breiðu', 'FVB-KVK-EFET')]
+        {('breiða', 'FVB-KK-EFET'), ('breiða', 'FVB-HK-EFET'), ('breiðu', 'FVB-KVK-EFET')}
     )
     bf_et = lambda b: ("ET" in b and "gr" not in b)
     assert(
