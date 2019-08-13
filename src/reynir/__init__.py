6d0a9fc321b5e5e14aac718a8ecab494cace8ebd
Vilhjalmur Thorsteinsson <vt@extrada.com>
2019-08-13 12:37:02 +0000

    Updated version to 1.8.0 in preparation for release

--- src/reynir/__init__.py
+++ src/reynir/__init__.py
@@ -44,7 +44,7 @@ from tokenizer import (
 __author__ = "Miðeind ehf."
 __copyright__ = "(C) 2019 Miðeind ehf."
 # Remember to update the version in doc/conf.py as well
-__version__ = "1.7.1"
+__version__ = "1.8.0"
 
 Abbreviations.initialize()
 Settings.read("config/ReynirPackage.conf")
