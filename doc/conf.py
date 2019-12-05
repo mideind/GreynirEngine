8c7d1569599e49d57b92a8d4d5ab270894633f8f
Vilhjalmur Thorsteinsson <vt@extrada.com>
2019-12-05 19:01:01 +0000

    Added favicon.ico

--- doc/conf.py
+++ doc/conf.py
@@ -115,6 +115,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_favicon = "_static/favicon.ico"
 
 # -- Options for HTMLHelp output ------------------------------------------
 
