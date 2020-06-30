871e4f9f42c4a6015138f4e96d482a62cb15f98d
Holado <holado@gmail.com>
2020-06-30 13:34:50 +0000

    Fixed errors

--- src/reynir/bintokenizer.py
+++ src/reynir/bintokenizer.py
@@ -1185,7 +1185,7 @@ def parse_phrases_3(token_stream, token_ctor):
                     or (token.kind == TOK.WORD and not token.val)
                     or (token.kind == TOK.WORD and token.val[0].ordfl == "entity")
                 )
-                and token.txt.is_title()
+                and token.txt.istitle()
                 and " " not in token.txt
             ):
                 # Upper-case word: Check next word
@@ -1204,7 +1204,7 @@ def parse_phrases_3(token_stream, token_ctor):
                         break
                     elif (
                         (next_token.kind == TOK.ENTITY or (next_token.kind == TOK.WORD and not token.val))
-                        and next_token.txt.is_title()
+                        and next_token.txt.istitle()
                     ):
                         entitytxt += " " + next_token.txt
                         next_token = next(token_stream)
