5f3d10373f25b8a5a7085b61de0c870b0ac2a624
Vilhjalmur Thorsteinsson <vt@extrada.com>
2020-10-07 00:12:52 +0000

    Added generation of PyPy wheels

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Build the reynir wheels on the CentOS5 base manylinux1 platform
+# Build the reynir wheels on the CentOS5/6 base manylinux1/manylinux2010 platform
 # This script should be executed inside the Docker container!
 # It is invoked indirectly from wheels.sh
 
@@ -11,8 +11,12 @@ for PYBIN in cp36 cp37; do
     "/opt/python/${PYBIN}-${PYBIN}m/bin/pip" wheel /io/ -w wheelhouse/
 done
 # Create wheels for Python >= 3.8
-for PYBIN in cp38; do
+for PYBIN in cp38 cp39; do
 	"/opt/python/${PYBIN}-${PYBIN}/bin/pip" wheel /io/ -w wheelhouse/
+done
+# Create wheels for PyPy3 (>=3.6)
+for PYBIN in /opt/pypy/pypy3.*/bin; do
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
--- wheels.sh
+++ wheels.sh
@@ -2,6 +2,7 @@
 echo "Building manylinux2010 wheels..."
 # Build manylinux2010 versions via a Docker CentOS6 image
 # See https://github.com/pypa/python-manylinux-demo/blob/master/.travis.yml
+# and https://github.com/pypy/manylinux
 mkdir -p /tmp/io
 chmod 777 /tmp/io
 chgrp docker /tmp/io
@@ -16,10 +17,10 @@ chgrp docker /tmp/io/wheelhouse
 cp -L ./* /tmp/io
 cp -L -r ./src/* /tmp/io/src
 cp -L -r ./test/* /tmp/io/test
-# Pull the latest pypa/manylinux2010 Docker image
-docker pull quay.io/pypa/manylinux2010_x86_64
+# Pull the latest pypywheels/manylinux2010 Docker image
+docker pull pypywheels/manylinux2010-pypy_x86_64
 # Run the Docker image
-docker run --rm -e PLAT=manylinux2010_x86_64 -it -v /tmp/io:/io quay.io/pypa/manylinux2010_x86_64 bash /io/build_wheels.sh
+docker run --rm -e PLAT=manylinux2010_x86_64 -it -v /tmp/io:/io pypywheels/manylinux2010-pypy_x86_64 bash /io/build_wheels.sh
 # Copy the finished wheels
 mkdir -p ./dist
 mv /tmp/io/wheelhouse/reynir* ./dist
